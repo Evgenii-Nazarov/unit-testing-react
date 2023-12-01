3f081e33d7967d2a75d60974e6d6973ae60cc59f
Evgenii Nazarov <evgeniinazarov@Evgeniis-MacBook-Pro.local>
2023-11-30 19:26:45 -0800

    mini update

--- src/utils/main.js
+++ src/utils/main.js
@@ -1,6 +1,6 @@
 import { countMeanCreditScore, countCreditScoreRatio } from './creditScore'
-import { countTotalSum, multiplyTwoNumbers, getIsBeneficiary } from './helper'
-import { getBuyingPotential, getPersonsStats } from './buyingPotential'
+import { countTotalSum } from './helper'
+import { getBuyingPotential } from './buyingPotential'
 
 const getCredirScoreRatio = (creditScores) => {
     const meanCreditScore = countMeanCreditScore(creditScores)
@@ -14,8 +14,8 @@ const getCredirScoreRatio = (creditScores) => {
  */
  const getTotalPotential = (persons) => {
     const personsPotentials = persons.map(pearsonData => {
-        const buyingPotential = getBuyingPotential(pearsonData)
-        return buyingPotential
+        const buyPotential = getBuyingPotential(pearsonData)
+        return buyPotential
     })
 
     const totalPotentials = countTotalSum(personsPotentials)
@@ -23,22 +23,7 @@ const getCredirScoreRatio = (creditScores) => {
     return totalPotentials
 }
 
-/**
-  * @param {Object} personData Information about the person.
-  * @param {number} personData.age The age of the person.
-  * @param {number} personData.yearIncome The year income of the person.
-  * @param {number} personData.monthlyExpenses The monthly expenses of the person.
-  * @param {number} personData.creditScore The credit score of the person.
-  * @param {string} personData.occupation The occupation of the person.
-  */
- const getBuyingPotential = (personData) => {
-    const { ageRatio, freeCash } = getPersonsStats(personData)
-    const buyingPotential = multiplyTwoNumbers(freeCash, ageRatio)
-
-    return buyingPotential
-}
-
 export {
     getCredirScoreRatio,
-    getTotalPotential
+    getTotalPotential,
 }
